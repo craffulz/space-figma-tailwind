90501d7a489c79fd1f9ba9be47112a5bd57d4f5c
zuza <115052199+craffulz@users.noreply.github.com>
2024-10-18 23:35:01 +0200

    cover center

--- space_web/src/pages/technology.jsx
+++ space_web/src/pages/technology.jsx
@@ -4,7 +4,7 @@ import Vehicles from "../components/Vehicles";
 
 const technology = () => {
   return (
-    <div className="flex flex-col bg-technology_mobile md:bg-technology_tablet lg:bg-technology bg-fixed md:min-h-screen overflow-hidden">
+    <div className="flex flex-col bg-technology_mobile md:bg-technology_tablet lg:bg-technology bg-cover bg-center bg-fixed md:min-h-screen overflow-hidden">
       <ScrollRestoration />
       <NavbarPicker />
       <Vehicles />
