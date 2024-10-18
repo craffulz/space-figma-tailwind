8114ce6bd20df9740f76fa8090f505eb0cee2379
zuza <115052199+craffulz@users.noreply.github.com>
2024-10-18 23:59:32 +0200

    heigh added to sm layout

--- space_web/src/pages/destination.jsx
+++ space_web/src/pages/destination.jsx
@@ -3,7 +3,7 @@ import NavbarPicker from "../components/NavbarPicker";
 import World from "../components/World";
 const destination = () => {
   return (
-    <div className="flex flex-col bg-destination_mobile md:bg-destination_tablet lg:bg-destination bg-fixed bg-cover bg-center min-h-screen w-full overflow-hidden justify-center items-center">
+    <div className="flex flex-col bg-destination_mobile md:bg-destination_tablet lg:bg-destination bg-fixed bg-cover bg-center h-[932px] md:min-h-screen w-full overflow-y-scroll md:overflow-hidden justify-center items-center">
       <ScrollRestoration/>
       <NavbarPicker />
       <World />
