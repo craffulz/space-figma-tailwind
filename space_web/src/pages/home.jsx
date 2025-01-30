e386315c5e267c1227a729743fab7cd605bf11da
zuza <115052199+craffulz@users.noreply.github.com>
2025-01-30 09:33:19 +0100

    fixing bg mobile bug

--- space_web/src/pages/home.jsx
+++ space_web/src/pages/home.jsx
@@ -8,7 +8,7 @@ const home = () => {
     <div
       className={`flex flex-col overflow-y-scroll md:overflow-hidden justify-between relative animate-fadeIn 
            w-full h-[932px] md:min-h-screen 
-           bg-home_mobile md:bg-home_tablet lg:bg-home md:bg-cover`}
+           bg-home_mobile md:bg-home_tablet lg:bg-home md:bg-cover bg-no-repeat bg-cover`}
     >
       <ScrollRestoration />
       <NavbarPicker />
